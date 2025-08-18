08a578afbb7251aab3c47d95a2dc4686819447d2
Shivraj Randive <shivrajrandive9281@gmail.com>
2025-08-18 17:32:40 +0530

    Delete PollyDemoApi directory

--- PollyDemoApi/PollyDemoApi/Data/TestItem.cs
+++
@@ -1,8 +0,0 @@
-namespace PollyDemoApi.Data
-{
-    public class TestItem
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-    }
-}
--- PollyDemoApi/PollyDemoApi/Pages/ Index.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace PollyDemoApi.Pages
-{
-    public class IndexModel : PageModel
-    {
-        public void OnGet() { }
-    }
-}
